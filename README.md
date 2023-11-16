54dfbbd659be00e38e35c47f26f18840b10a726f
freydisxuan <freydisxuan@gmail.com>
2023-11-16 15:04:56 +0000

    deli - ice spice

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-### Verkefni 10 TÖL07G
-Notaði React :D
+### Verkefni 10 TÖL07G í ReactJS
+
+Síða keyrð á `localhost:3000` með `npm run start`
+
+Linter keyrður með `npm run lint`
+
+Síðan notar GET-köll í endapunktinn `https://pokeapi.co/api/v2/pokemon/{id/name}` til þess að sækja JSON gögn um samsvarandi Pokémon.
+
+Fyrir random virknina framkallar síðan slembitölu á bilinu 1-1016 og kallar á endapunktinn með töluna sem ID. 
+
+[PokeAPI Docs](https://pokeapi.co/docs/v2)
+
+[Pokémon Cursor](https://custom-cursor.com/en/collection/minimal-style/minimal-poke-ball)
--- src/App.js
+++ src/App.js
@@ -3,38 +3,41 @@ import axios from 'axios';
 import _ from 'lodash';
 import { useCallback, useState, useMemo, useEffect } from 'react';
 import './App.css';
+import React from 'react';
 
 function App() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState('');
   const [image, setImage] = useState(logo);
+  const [color, setColor] = useState('#26de81');
 
-// const typeColorMap = {
-//  'bug': "#26de81",
-//  'dragon': "#ffeaa7",
-//  'electric': "#fed330",
-//  'fairy': "#FF0069",
-//  'fighting': "#30336b",
-//  'fire': "#f0932b",
-//  'flying': "#81ecec",
-//  'grass': "#00b894",
-//  'ground': "#EFB549",
-//  'ghost': "#a55eea",
-//  'ice': "#74b9ff",
-//  'normal': "#95afc0",
-//  'poison': "#6c5ce7",
-//  'psychic': "#a29bfe",
-//  'rock': "#2d3436",
-//  'water': "#0190FF",
-//  'dark': "#6A411C",
-//  'steel':"#97A2B6",
-//  };
-
-//  const typeColor = typeColorMap[primaryType] || "#ffffff";
-//  const secondaryColor = typeColorMap[secondaryType] || "#ffffff";
+  const typeColorMap = useMemo(() => {
+    return {
+      bug: "#abe0b3",
+      dragon: "#ffeaa7",
+      electric: "#fed330",
+      fairy: "#ffadfc",
+      fighting: "#30336b",
+      fire: "#f0932b",
+      flying: "#81ecec",
+      grass: "#00b894",
+      ground: "#EFB549",
+      ghost: "#a55eea",
+      ice: "#74b9ff",
+      normal: "#95afc0",
+      poison: "#6c5ce7",
+      psychic: "#a29bfe",
+      rock: "#2d3436",
+      water: "#0190FF",
+      dark: "#6A411C",
+      steel:"#97A2B6",
+    };
+  }, []);
 
   useEffect(() => {
     generateRandom();
+    // eslint recommends dependency that causes intfinite re-render :)
+    // eslint-disable-next-line 
   }, []);
 
   const generateInfo = useMemo(() => {
@@ -47,6 +50,8 @@ function App() {
       data.types.forEach(t => {
         types.push(_.startCase(t.type.name));
       });
+      console.log(typeColorMap[data.types[0].type.name]);
+      setColor(typeColorMap[data.types[0].type.name]);
     }
     if (data.abilities.length > 0) {
       data.abilities.forEach(a => {
@@ -55,13 +60,13 @@ function App() {
     }
     const typeTitle = types.length > 1 ? 'Types:' : 'Type:';
     const abilityTitle = abilities.length > 1 ? 'Abilities:' : 'Ability:';
-    // const themeColor = typeColor[data.types[0].type.name];
     return (
+       // eslint-disable-next-line
       <>
-        <p className='poke-name'>{data.name[0].toUpperCase() + data.name.slice(1)}</p>
+        <p className='poke-name'>{_.startCase(data.name.replace('-',' '))}</p>
         <p className='hp'>HP: {data.stats[0]['base_stat']}</p>
         {types.length > 0 && 
-          <p className='types'>{typeTitle} {types.join(', ')}</p>
+          <p className='types' style={{ backgroundColor: color, color: color === '#6A411C' || color === '#2d3436' || color === '#30336b' ? 'white' : 'black' }}>{typeTitle} {types.join(', ')} </p>
         }
         {abilities.length > 0 && 
           <p>{abilityTitle} {abilities.join(', ')}</p>
@@ -72,7 +77,7 @@ function App() {
        </section>
       </>
     )
-  }, [data])
+  }, [data, color, typeColorMap])
   
 
   const updateSearch = useCallback((e) => {
@@ -106,23 +111,23 @@ function App() {
     }).catch(err => {
       alert(err);
     })
-  }, [data]);
+  }, []);
 
   
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pokémon search</h1>
-        <div className='card'>
-        <img src={image} className="App-logo" alt="logo" />
-        <div className="ButtonWrapper">
-          <input type="text" value={search} onChange={(e) => updateSearch(e)} onKeyDown={(e) => e.key === 'Enter' && getData(search)} placeholder="Enter Pokémon name"/>
-          <button onClick={() => generateRandom()}>Generate random pokémon!</button>
-        </div>
-        <div className="InfoWrapper">
-          {generateInfo}
-        </div>
+        <h1>Pokémon Search</h1>
+        <div className='card' style={{ background: `radial-gradient(circle at 50% 0%, ${color} 36%, rgb(255, 255, 255) 36%)` }}>
+          <img src={image} className="App-logo" alt="logo" />
+          <div className="ButtonWrapper">
+            <input type="text" spellCheck={false} value={search} onChange={(e) => updateSearch(e)} onKeyDown={(e) => e.key === 'Enter' && getData(search)} placeholder="Enter Pokémon Name"/>
+            <button onClick={() => generateRandom()}>Generate Random Pokémon!</button>
+          </div>
+          <div className="InfoWrapper">
+            {generateInfo}
+          </div>
         </div>
       </header>
     </div>
